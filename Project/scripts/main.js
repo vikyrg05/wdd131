b227ce0cb41b5250dcfb7dd9718c0f5e5ff73026
vikyrg05 <vikyrg05@gmail.com>
2026-02-14 09:09:45 -0400

    Improvements made to tha page

--- Project/scripts/main.js
+++ Project/scripts/main.js
@@ -6,17 +6,17 @@ const lastModified = new Date(document.lastModified);
 const formattedDate = `${lastModified.getDate()}/${lastModified.getMonth() + 1}/${lastModified.getFullYear()}`;
 
 if (footerYear) {
-footerYear.textContent = `\u00A9 ${currentYear} Virginia Rocha - Daily Student Wellness Page`;
+    footerYear.textContent = `\u00A9 ${currentYear} Virginia Rocha - Daily Student Wellness Page`;
 }
-    
+
 if (footerModified) {
-footerModified.textContent = `Last Modification: ${formattedDate}`;
+    footerModified.textContent = `Last Modification: ${formattedDate}`;
 }
-    
+
 const toggleButton = document.getElementById("menu-toggle");
 const navMenu = document.querySelector("#nav-menu ul");
 
-if (toggleButton && navMenu){
+if (toggleButton && navMenu) {
     toggleButton.addEventListener("click", () => {
         navMenu.classList.toggle("show");
     });
@@ -30,15 +30,22 @@ if (form) {
         event.preventDefault();
 
         const name = document.getElementById("name").value;
+        const email = document.getElementById("email").value;
 
-        responseMessage.textContent = `Thank you, ${name}! Your message has been received.`
+        const studentMessage = {
+            name: name,
+            email: email,
+            date: new Date().toLocaleDateString()
+        };
+
+        const messages = JSON.parse(localStorage.getItem("messages")) || [];
+
+        messages.push(studentMessage);
+
+        localStorage.setItem("messages", JSON.stringify(messages));
+
+        responseMessage.textContent = `Thank you, ${name}! Your message has been received.`;
 
         form.reset();
     });
-
-    const messages = JSON.parse(localStorage.getItem("messages")) || [];
-
-    messages.push({ name: name });
-
-    localStorage.setItem("messages", JSON.stringify(messages));
 }
